366837f24cfe06201d0b34daf2ef5787674f4251
Praveen <praveenreddyshetty@gmail.com>
2022-12-27 19:59:52 +0530

    commit toString

---
+++ src/libraries/reddy/TostringExample.java
@@ -0,0 +1,22 @@
+package libraries.reddy;
+/*1.toString() method:
+        ->toString() will return String representation of an Object.
+        Syntex:public String toString()
+        ->When we print the reference variable or Object reference implicitly toString() gets called.
+        ->toString() will returns the String representation of an Object in the below formate:
+        --> FullyQualifiedClass@HexaDecimalvalueOfHAshcode
+
+ */
+
+public class TostringExample {
+    public String toString(){
+        return "toString";
+    }
+
+    public static void main(String[] args) {
+        TostringExample to = new TostringExample();
+        System.out.println(to);//implicitly calling tostring().
+        System.out.println(to.toString());//Explicitly calling tostring().
+    }
+
+}
