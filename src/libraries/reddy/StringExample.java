2c7a7521a7fd62f42b5b90e3b3bdfde737e93cb8
Praveen <praveenreddyshetty@gmail.com>
2022-12-28 15:41:28 +0530

    String notes and example

---
+++ src/libraries/reddy/StringExample.java
@@ -0,0 +1,56 @@
+package libraries.reddy;
+/*
+--#Note,
+   --@ String class implements the following interfaces:->
+      1)Serializable
+      2)Comparable
+      3)char sequence
+   --> String class also implicitly inherits the object class and has Overridden 3 methods:->
+       1)toString()
+       2)hashCode()
+       3)equals()
+   1)toString() method should have return the String  representation of an object
+       but in String class toString() method is overridden  to return a set of character.
+   2)hashCode() method should have return a unique random no. but in String class
+       hashCode() method is overridden to return ASCII value.
+   3)equals() method should have compare the address or reference of two object
+       but in String class equals() method is overridden to compare the content of two String object.
+
+   -->( = = ) will compare the address or reference.
+   -->equals() will compare the content or date.
+ */
+//Example:
+/*public class StringExample {
+    public static void main(String[] args)
+    {
+        StringExample s= new StringExample();
+
+        System.out.println(s);//libraries.reddy.StringExample@58372a00
+        System.out.println(s.toString());//libraries.reddy.StringExample@58372a00
+        System.out.println(s.hashCode());//1480010240
+
+        StringExample s1= new StringExample();
+        StringExample s2= new StringExample();
+
+        System.out.println(s1.equals(s2));//false
+        }
+}
+*/
+public class StringExample {
+    public static void main(String[] args)
+    {
+        String s= new String();
+
+        System.out.println(s);// null
+        System.out.println(s.toString());// null
+        System.out.println(s.hashCode());//0
+
+        String s1= new String();
+        String s2= new String();
+//        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));//true
+
+    }
+
+}
+
