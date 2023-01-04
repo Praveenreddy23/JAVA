c897d5f18f68b3ed0f0a6689a029f247fee2e6bb
Praveen <praveenreddyshetty@gmail.com>
2023-01-04 13:19:44 +0530

    Exception Handling

--- src/ExceptionHandling/TryAndCatch.java
+++ src/ExceptionHandling/TryAndCatch.java
@@ -19,5 +19,43 @@ public class TryAndCatch {
             System.out.println("Super Class Exception Handler");
         }
 
+        //printStackTrace() : This method is used to get the complete info about the Exception.
+        System.out.println("Hello");
+        try
+        {
+            System.out.println(10/0);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("Bye");
+
+       //getMessage() :  This method is used to return a small message about the exception occurred.
+        try{
+            System.out.println(10/0);
+        }catch(Exception e){
+            System.out.println(e.getMessage());// by zero
+            String message = e.getMessage();
+            System.out.println(message);// by zero
+        }
+
+//       Finally-block : Is a block of code which gets executed all the time.
+        try
+        {
+            System.out.println(10/0);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("invalid denominator");
+        }
+        finally
+        {
+            System.out.println("inside finally block");
+        }
+
+
+
+
+
     }
 }
