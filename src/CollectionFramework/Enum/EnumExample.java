f3a0d2deb88175c1427d71ef8a6767db80949f19
Praveen <praveenreddyshetty@gmail.com>
2023-01-18 18:34:38 +0530

    Collections

---
+++ src/CollectionFramework/Enum/EnumExample.java
@@ -0,0 +1,14 @@
+package CollectionFramework.Enum;
+
+public class EnumExample {
+    enum Name{
+        praveen,Reddy,Prabhas,Sallar;
+    }
+
+    public static void main(String[] args) {
+        Name name = Name.Prabhas;
+        System.out.println(name);
+    }
+}
+
+
