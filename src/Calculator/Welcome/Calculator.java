aea2d841a0dec5c5ac4e584a2b35e3c3a9b47f80
Praveen <praveenreddyshetty@gmail.com>
2023-02-01 12:01:01 +0530

    Update

--- src/Calculator/Welcome/Calculator.java
+++ src/Calculator/Welcome/Calculator.java
@@ -7,6 +7,7 @@ public interface Calculator {
     int sub(int a, int b);
     int mul(int a, int b);
     int div(int a, int b);
+    String invalid();
 
 }
  class CalculatorIMPL implements Calculator {
@@ -27,7 +28,14 @@ public interface Calculator {
 
     @Override
     public int div(int a, int b) {
-        return a/b;
+        if (b!=0){
+            return a/b;
+        }
+        return 0;
+    }
+    @Override
+     public String invalid(){
+        return "Enter valid input";
     }
 }
 
@@ -40,8 +48,9 @@ public interface Calculator {
         Calculator c = new CalculatorIMPL();
 
         System.out.println("Wel come to calculator...");
-        System.out.println("---------------------");
+
         while(true){
+            System.out.println("---------------------");
             System.out.println("1:Addition\n2:Subtraction\n3:Multiplication\n4:Division\n5:Exit");
             System.out.println("Enter choice");
             int choice = scan.nextInt();
@@ -56,24 +65,37 @@ public interface Calculator {
             }
 
             switch (choice) {
-                case 1:
+                case 1 ->
 //                int sum = c.add(a,b);
-                    System.out.println("Sum is: " + c.add(a, b));break;
-
-                case 2:
-                    System.out.println("Difference: " + c.sub(a, b));break;
-                case 3:
-                    System.out.println("Product: " + c.mul(a, b));break;
-                case 4:
-                    System.out.println("Quotient: " + c.div(a, b));break;
-                case 5:
+                        System.out.println("Sum of : " + a + " &" + b + " is" + c.add(a, b));
+                case 2 -> System.out.println("Difference of : " + a + " &" + b + " is" + c.sub(a, b));
+                case 3 -> System.out.println("Product of : " + a + " &" + b + " is" + c.mul(a, b));
+                case 4 -> {
+                    int res = c.div(a,b);
+                    if (res!=0){
+                        System.out.println("Quotient of : " + a + " &" + b + " is" + res);
+                    }
+                    else {
+                        System.out.println("Kindly do not divide by 0");
+                    }
+                }
+                case 5 -> {
                     System.out.println("Thank You!!");
                     System.exit(0);
-                default:
-                    System.out.println("Invalid choice");
+                }
+                default -> System.out.println(c.invalid());
             }
         }
 
     }
 }
+/*
+RULES FOR ABSTRACTION
+->INTERFACE WITH ABSTRACT METHOD
+->IS-A-RELATIONSHIP
+->UPCASTING
+->METHOD OVERRIDE
+
+*/
+
 
